f96a0756edba8aae39c24d068ef73f82c7e53997
Nathalie Beiter <nathalie.beiter@stud.hs-flensburg.de>
2022-12-21 10:19:51 +0100

    Adjustments in Controller.js, Layout.html
    Deleted single export functions for artists in controller
    Added footer to layout.html

--- FD-HA-nabe1026-nobe1803/src/controller.js
+++ FD-HA-nabe1026-nobe1803/src/controller.js
@@ -34,14 +34,6 @@ export function datenschutzerklärung(ctx) {
   return ctx;
 }
 
-export function dourouedouroue(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("DouroueDouroue.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
 export function formular(ctx) {
   debug("@index. ctx %O", ctx.request.url);
   ctx.response.body = ctx.nunjucks.render("Formular.html", {});
@@ -76,49 +68,9 @@ export function lineup(ctx) {
   return ctx;
 }
 
-export function onsonsweeney(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("OnsonSweeney.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
-export function shownfurcotte(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("ShownFurcotte.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
-export function slevemcdichael(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("SleveMcdichael.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
-export function thechamgerlains(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("TheChamgerlains.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
 export function tickets(ctx) {
   debug("@index. ctx %O", ctx.request.url);
   ctx.response.body = ctx.nunjucks.render("tickets.html", {});
-  ctx.response.status = 200;
-  ctx.response.headers["content-type"] = "text/html";
-  return ctx;
-}
-
-export function toddbonzalez(ctx) {
-  debug("@index. ctx %O", ctx.request.url);
-  ctx.response.body = ctx.nunjucks.render("ToddBonzalez.html", {});
   ctx.response.status = 200;
   ctx.response.headers["content-type"] = "text/html";
   return ctx;
